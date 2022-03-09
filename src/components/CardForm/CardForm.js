08754bf4364934de5ee06df5587001972091bd28
Damian Osypinski <d.osypinski@gmail.com>
2022-03-09 20:58:44 +0100

    add ADD_CARD

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,12 +2,14 @@ import Button from '../Button/Button.js';
 import TextInput from '../TextInput/TextInput.js';
 import styles from './CardForm.module.scss';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const CardForm = props => {
+    const dispatch = useDispatch();
     const [title, setTitle] = useState('');
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title}, props.columnId);
+        dispatch({ type: 'ADD_CARD', payload: {title, columnId: props.columnId} });
         setTitle('');
     };
 
