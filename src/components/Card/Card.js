fca9e06fdeb86b37563430afe428a2f07cc1ac1b
Damian Osypinski <d.osypinski@gmail.com>
2022-03-26 19:59:37 +0100

    add removeCard

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,9 @@
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
-import { favoriteToggle } from 'src/redux/cardsRedux';
+import { favoriteToggle, removeCard } from 'src/redux/cardsRedux';
 import { useSelector } from 'react-redux';
 
 const Card = props => {
@@ -14,11 +14,19 @@ const Card = props => {
         e.preventDefault();
         dispatch(favoriteToggle(cardId))
     };
+    const removeHandler = e => {
+        e.preventDefault();
+        dispatch(removeCard(cardId))
+    }
     const card = useSelector(state => state.cards.filter(card => card.id === cardId));
     const isFavorite = card[0].isFavorite;
     return (
         <li className={styles.card}>{props.title}
-        <FontAwesomeIcon onClick={toggleHandler} className={clsx(styles.icon,  {[styles.isFavorite] : isFavorite })} icon={faStar} /></li>
+            <div>
+                <FontAwesomeIcon onClick={toggleHandler} className={clsx(styles.icon,  {[styles.isFavorite] : isFavorite })} icon={faStar} />
+                <FontAwesomeIcon onClick={removeHandler} className={styles.icon} icon={faTrash} />
+            </div>
+        </li>
     );
 };
 
